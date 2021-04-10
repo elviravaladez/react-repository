f45365db6f1d85c5c84de9e2db4e8b51f36b2d83
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-09 23:31:48 -0500

    add: cards to the App function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
     return (
         <div>
             <h1>Hello, friends! This is my first time using React!</h1>
+
+            <div className="person">
+                <h1>Jane Doe</h1>
+                <p>School: Codeup</p>
+                <p>Cohort: Jupiter</p>
+            </div>
+
+            <div className="person">
+                <h1>John Doe</h1>
+                <p>School: Codeup</p>
+                <p>Cohort: Jupiter</p>
+            </div>
+
+            <div className="person">
+                <h1>Sam Doe</h1>
+                <p>School: Codeup</p>
+                <p>Cohort: Jupiter</p>
+            </div>
         </div>
     );
 }
