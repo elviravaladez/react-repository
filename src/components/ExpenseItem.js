34dfbb9319749e53281366c628141e48520d6e00
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-16 16:13:30 -0500

    style: ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>April 16th, 2021</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <div>$250.35</div>
+                <div className="expense-item__price">$250.35</div>
             </div>
         </div>
     );
 }
+
 export default ExpenseItem;
