90cb631dc36821469e3f4014cf5c99f892b70d56
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-16 16:31:05 -0500

    add: const for date, title, and amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 3, 16);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>April 16th, 2021</div>
+            <div>{expenseDate.toLocaleDateString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$250.35</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
