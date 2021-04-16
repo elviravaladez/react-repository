90eac084e1159c4796359bb37c70724b08a6702f
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-16 16:05:54 -0500

    add: intial start of application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import './App.css';
-import ReactDOM from "react-dom";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App(props) {
-    //JSX
-    //Babel allows us to use this special syntax that looks like HTML, but it isn't
     return (
-        <div className="person">
-            <h1>{props.name}</h1>
-            <p>{props.school}</p>
-            <p>{props.cohort}</p>
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem/>
         </div>
     );
 }
-
-var app = (
-    <div>
-        <App name="John" school="Codeup" cohort="Jupiter" />
-        <App name="Jane" school="Codeup" cohort="Jupiter" />
-    </div>
-);
-
-ReactDOM.render(app, document.querySelector('#root'));
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>April 16th, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$250.35</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem;
