236c44e1822c98cac62fcf66e0837abe59b8c507
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-28 22:29:22 -0500

    add: state comments

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; //importing a function provided by the React library
+//useState is a React Hook: allows us to define values as state where changes to these value should reflect in the component function being called again
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
     const [title, setTitle] = useState(props.title);
+    //useState() returns an array
     //useState ALWAYS returns two elements
     //current state value and function for updating that value
+    //useState registers some state value for a specific component INSTANCE
+
+    //Whenever state changes, it is only the component function, and that SPECIFIC INSTANCE where the component is being use where that component is being used where React will re-evaluate.
+
+    //best practice to name the two elements by useState()
 
     const clickHandler = () => {
         setTitle('Updated!');
