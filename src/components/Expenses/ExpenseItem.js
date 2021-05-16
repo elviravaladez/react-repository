8b193188a14df16d2dd55e0459f274648c77280a
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-16 16:03:32 -0500

    add: conditional return statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; //importing a function provided by the React library
+import React, {useState} from "react"; //importing a function provided by the React library
 //useState is a React Hook: allows us to define values as state where changes to these value should reflect in the component function being called again
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,15 +6,17 @@ import './ExpenseItem.css';
 
 const ExpenseItem = props => {
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">
-                    ${props.amount}
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">
+                        ${props.amount}
+                    </div>
                 </div>
-            </div>
-        </Card>
+            </Card>
+        </li>
     );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './Expenses.css';
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 
@@ -15,18 +15,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filterYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
     return (
         <div>
             <Card className="expenses">
@@ -35,7 +23,7 @@ const Expenses = (props) => {
                     selected={filterYear}
                     onSaveExpenseFilter={saveExpenseFilterHandler}
                 />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
