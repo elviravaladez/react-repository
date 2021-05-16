23c0275231071166ae2dcb8cd8d59272e8ed7203
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-16 15:38:46 -0500

    feat: rendered items by date with filter()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,10 @@ const Expenses = (props) => {
         setFilterYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filterYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
@@ -19,7 +23,7 @@ const Expenses = (props) => {
                     selected={filterYear}
                     onSaveExpenseFilter={saveExpenseFilterHandler}
                 />
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
